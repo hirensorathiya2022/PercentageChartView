a886c6e5a24843dac2e62a08900859c8102628dc
Hiren Sorathiya <hirensorthiya2022@gmail.com>
2025-04-09 16:53:24 +0530

    1.Update a library aar file. 1.Solved minor testing issue.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation("com.github.hirensorathiya2022:PercentageChartView:1.0.1")
 }
